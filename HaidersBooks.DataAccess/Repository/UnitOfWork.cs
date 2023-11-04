3db39def22508c27a942653748a3729f88b4c4ba
Haider Ali <Haider Ali@DESKTOP-L3IJC2A>
2023-11-03 20:23:42 -0400

    Created Isp files

---
+++ HaidersBooks.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using HaiderBookStore.DataAccess.Data;
+using HaidersBooks.DataAccess.Repository.IRespository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaidersBooks.DataAccess.Repository
+{
+    public class UnitOfWork // make the method public to access the class
+    {
+        private readonly ApplicationDbContext _db; // the using statement
+
+        public UnitOfWork(ApplicationDbContext db)  // constructor to use DI and inject into the repositories
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+
+}
